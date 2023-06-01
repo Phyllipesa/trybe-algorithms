9b68f21fc0e81185066472ea72ea7a74673885e0
Phyllipesa <phyllipe.sjc@hotmail.com>
2023-06-01 18:53:56 -0300

    req 2 ok

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 3)
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(3, 'b')
+    assert encrypt_message('camada', 3) == 'mac_ada'
+    assert encrypt_message('camada', 4) == 'ad_amac'
+    assert encrypt_message('camada', 9) == 'adamac'
+    assert encrypt_message('camada', -1) == 'adamac'
+    assert encrypt_message('camada', 5) == 'damac_a'
