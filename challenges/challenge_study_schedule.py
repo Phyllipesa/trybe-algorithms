f5295d864f7537dd435a922a46e998ba1a23e13f
Phyllipesa <phyllipe.sjc@hotmail.com>
2023-06-01 18:04:38 -0300

    req 1 ok

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    result = 0
+
+    try:
+
+        for each in permanence_period:
+            if (target_time >= each[0] and target_time <= each[1]):
+                result += 1
+        return result
+
+    except Exception:
+        return None
+
     raise NotImplementedError
